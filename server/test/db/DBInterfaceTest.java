b6e724b88eb181320492a340194107655f7b7fb5
Bradley Smith <bs2113@pixel37.doc.ic.ac.uk>
2015-05-22 17:51:26 +0100

    Checkedout

--- server/test/db/DBInterfaceTest.java
+++ server/test/db/DBInterfaceTest.java
@@ -4,10 +4,16 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.sql.SQLException;
+
 import org.junit.Test;
 
+import req.SessionRequest;
+import req.UserRequest;
+import resp.UserResponse;
+
 public class DBInterfaceTest {
-  @Test
+  /*@Test
   public void queryFunctionMakesQueryToTheDataBaseUsingTheSpecifiedObject() {
     LoginQuery query = new LoginQuery("Hello");
     DBInterface db = new DBInterface();
@@ -24,6 +30,21 @@ public class DBInterfaceTest {
     SessionInsert s = new SessionInsert("123", 2);
     DBInterface db = new DBInterface();
     db.insert(s);
+  }*/
+	
+	
+  DBInterface db = new DBInterface();
+  
+  @Test
+  public void test() throws SQLException, UserNotFoundException {
+	UserResponse ur = db.verifyUser(new UserRequest("bob@thebuilder.com", "HELLAKEDWQ"));
+	System.out.println(ur.getEmail() + "\n" + ur.getPassword() + "\n" + ur.getUserId());
+  }
+  
+  @Test
+  public void test1() throws SQLException {
+	  DBInterface db = new DBInterface();
+	  System.out.println(db.addSession(new SessionRequest(1, "Hellloo")));
   }
 
 }
