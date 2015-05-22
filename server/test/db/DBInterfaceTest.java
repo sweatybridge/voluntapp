838dc7257cc09e590476d06ec0448b350db266c6
Piotr Chabierski <pc3813@pixel39.doc.ic.ac.uk>
2015-05-22 17:49:03 +0100

    Added a query to database for the user's ID corresponding to the session.

--- server/test/db/DBInterfaceTest.java
+++ server/test/db/DBInterfaceTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
+import java.sql.SQLException;
+
 import org.junit.Test;
 
 public class DBInterfaceTest {
@@ -11,11 +13,12 @@ public class DBInterfaceTest {
   public void queryFunctionMakesQueryToTheDataBaseUsingTheSpecifiedObject() {
     LoginQuery query = new LoginQuery("Hello");
     DBInterface db = new DBInterface();
-    db.query(query);
     try {
+      db.query(query);
       assertEquals(query.getPassword(), "HELLO");
       assertTrue(query.getID() == 3);
-    } catch (UserNotFoundException e) {
+    } catch (Exception e) {
+      fail("ERROR");
     }
   }
   
@@ -23,7 +26,25 @@ public class DBInterfaceTest {
   public void insertAddsDataToTheSpecifiedTable() {
     SessionInsert s = new SessionInsert("12345", 2);
     DBInterface db = new DBInterface();
-    db.insert(s);
+    try {
+      db.insert(s);
+    } catch (SQLException e) {
+      fail("ERROR");
+    }
+  }
+  
+  @Test
+  public void sessionQueryQuerriesTheUserID() {
+    DBInterface db = new DBInterface();
+    SessionQuery s = new SessionQuery("123");
+    Integer user = null;
+    try {
+      db.query(s);
+      user = s.getUserID();
+    } catch (Exception e) {
+      fail("Should not have thrown an exception!");
+    }
+    assertTrue(user == 2);
   }
 
 }
