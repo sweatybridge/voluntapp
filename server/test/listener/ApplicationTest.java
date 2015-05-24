f67e167df95b1af20232d00ef16af9180ba36120
Han Qiao <qh812@imperial.ac.uk>
2015-05-24 14:18:54 +0100

    moved database connection parameters out of database interface

---
+++ server/test/listener/ApplicationTest.java
@@ -0,0 +1,19 @@
+package listener;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class ApplicationTest {
+
+  @Test
+  public void contextInitializedMapsServletsToUrl() {
+    fail("Not yet implemented.");
+  }
+
+  @Test
+  public void contextDestroyedClosesDatabaseConnection() {
+    fail("Not yet implemented.");
+  }
+
+}
