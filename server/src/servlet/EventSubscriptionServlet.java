e098922cc85e5d311c11f4bf802abb06ce2e9528
Piotr Chabierski <pc3813@line32.doc.ic.ac.uk>
2015-05-27 19:57:59 +0100

    Changed the EventServlet to return success message on successful Insert/Delete/Update.

--- server/src/servlet/EventSubscriptionServlet.java
+++ server/src/servlet/EventSubscriptionServlet.java
@@ -1,6 +1,8 @@
 package servlet;
 
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import com.google.gson.Gson;
 
@@ -17,6 +19,32 @@ public class EventSubscriptionServlet extends HttpServlet {
     this.db = db;
   }
   
+  /**
+   * Given the ID of an event, return the number of people attending the event.
+   */
+  @Override
+  public void doGet(HttpServletRequest request, HttpServletResponse response) {
+    
+  }
   
-
+  /**
+   * Given the ID of an event, register the user's subscription to the event.
+   */
+  @Override
+  public void doPost(HttpServletRequest request, HttpServletResponse response) {
+    
+  }
+  
+  @Override
+  public void doPut(HttpServletRequest request, HttpServletResponse response) {
+    
+  }
+  
+  /**
+   * Given the ID of the event, delete user's subscription to the event.
+   */
+  @Override
+  public void doDelete(HttpServletRequest request, HttpServletResponse response) {
+    
+  }
 }
