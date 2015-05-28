fec0d01e1b47e69431577846e7288b0cc3472f08
Han Qiao <qh812@imperial.ac.uk>
2015-05-28 02:16:07 +0100

    corrected tests for auth filter

--- server/test/filter/AuthorizationFilterTest.java
+++ server/test/filter/AuthorizationFilterTest.java
@@ -1,6 +1,14 @@
 package filter;
 
+import static org.mockito.Matchers.any;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.sql.SQLException;
 
 import javax.servlet.FilterChain;
@@ -14,17 +22,13 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import resp.ErrorResponse;
+import resp.Response;
 import resp.SessionResponse;
-
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyInt;
-import static org.mockito.Mockito.*;
-
 import db.DBInterface;
 import exception.SessionNotFoundException;
 
 public class AuthorizationFilterTest {
-  
+
   @Mock
   HttpServletRequest req;
   @Mock
@@ -33,7 +37,9 @@ public class AuthorizationFilterTest {
   FilterChain chain;
   @Mock
   DBInterface db;
-  
+
+  private static final String TEST_SESSION_ID = "123456";
+
   private AuthorizationFilter filter;
 
   @Before
@@ -41,36 +47,102 @@ public class AuthorizationFilterTest {
     MockitoAnnotations.initMocks(this);
     filter = new AuthorizationFilter(db);
   }
-  
+
   @Test
-  public void filterReturnsAnErrorResponseWhenNoSessionIDWasFoundInBD() 
-      throws SQLException, SessionNotFoundException, 
-      IOException, ServletException {
-    when(db.getSession(any(String.class))).thenThrow(
+  public void filterAllowsUnauthenticatedAccessToRegistrationPage()
+      throws IOException, ServletException {
+    // Set up request
+    when(req.getRequestURI()).thenReturn("/api/user");
+    when(req.getMethod()).thenReturn("POST");
+
+    // Method under test
+    filter.doFilter(req, resp, chain);
+
+    // Fake session is installed
+    verify(req).setAttribute(eq(SessionResponse.class.getSimpleName()),
+        any(SessionResponse.class));
+    verify(chain).doFilter(req, resp);
+  }
+
+  @Test
+  public void filterAllowsUnauthenticatedAccessToLoginPage()
+      throws IOException, ServletException {
+    // Set up request
+    when(req.getRequestURI()).thenReturn("/api/session");
+    when(req.getMethod()).thenReturn("POST");
+
+    // Method under test
+    filter.doFilter(req, resp, chain);
+
+    // Fake session is installed
+    verify(req).setAttribute(eq(SessionResponse.class.getSimpleName()),
+        any(SessionResponse.class));
+    verify(chain).doFilter(req, resp);
+  }
+
+  @Test
+  public void filterStopsRequestsWithoutAuthorizationHeaderImmediately()
+      throws IOException, ServletException {
+    // Method under test
+    filter.doFilter(req, resp, chain);
+
+    // Error status is returned without passing to json filter
+    verify(resp).setStatus(HttpURLConnection.HTTP_UNAUTHORIZED);
+    verify(chain, never()).doFilter(req, resp);
+  }
+
+  @Test
+  public void filterReturnsAnErrorResponseWhenNoSessionIDWasFoundInBD()
+      throws SQLException, SessionNotFoundException, IOException,
+      ServletException {
+    // Sets up request
+    when(req.getHeader("Authorization")).thenReturn(TEST_SESSION_ID);
+    when(db.getSession(TEST_SESSION_ID)).thenThrow(
         new SessionNotFoundException());
+
+    // Method under test
     filter.doFilter(req, resp, chain);
-    verify(req, times(1)).setAttribute(
-        eq(SessionResponse.class.getSimpleName()), any(ErrorResponse.class));
+
+    // Request propagates down filter chain
+    verify(req, times(1)).setAttribute(eq(Response.class.getSimpleName()),
+        any(ErrorResponse.class));
+    verify(chain).doFilter(req, resp);
   }
-  
+
   @Test
-  public void filterReturnsAnErrorResponseWhenDBErrorOccurs() throws 
-      IOException, ServletException, SQLException, SessionNotFoundException {
-    when(db.getSession(any(String.class))).thenThrow(new SQLException());
+  public void filterReturnsAnErrorResponseWhenDBErrorOccurs()
+      throws IOException, ServletException, SQLException,
+      SessionNotFoundException {
+    // Sets up request
+    when(req.getHeader("Authorization")).thenReturn(TEST_SESSION_ID);
+    when(db.getSession(TEST_SESSION_ID)).thenThrow(new SQLException());
+
+    // Method under test
     filter.doFilter(req, resp, chain);
-    verify(req, times(1)).setAttribute(
-        eq(SessionResponse.class.getSimpleName()), any(ErrorResponse.class));
+
+    // Request propagates down filter chain
+    verify(req, times(1)).setAttribute(eq(Response.class.getSimpleName()),
+        any(ErrorResponse.class));
+    verify(chain).doFilter(req, resp);
   }
-  
+
   @Test
-  public void filterSearchesForSessionIDInDBAndAddsAnAttributeToRequest() 
-      throws SQLException, SessionNotFoundException, IOException, 
-        ServletException {
-    SessionResponse sessionResp = mock(SessionResponse.class);
-    when(db.getSession(any(String.class))).thenReturn(sessionResp);
+  public void filterSearchesForSessionIDInDBAndAddsAnAttributeToRequest()
+      throws SQLException, SessionNotFoundException, IOException,
+      ServletException {
+    SessionResponse session = new SessionResponse(TEST_SESSION_ID);
+
+    // Sets up request
+    when(req.getHeader("Authorization")).thenReturn(TEST_SESSION_ID);
+    when(db.getSession(TEST_SESSION_ID)).thenReturn(session);
+
+    // Method under test
     filter.doFilter(req, resp, chain);
-    verify(req, times(1)).setAttribute(
-        SessionResponse.class.getSimpleName(), sessionResp);
+
+    // Request propagates
+    verify(req, times(1)).setAttribute(SessionResponse.class.getSimpleName(),
+        session);
+    verify(chain).doFilter(req, resp);
   }
-  
+
 }
