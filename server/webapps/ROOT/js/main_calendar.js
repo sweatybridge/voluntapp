fe6c28d98519d18503d8b6c7edfcf41437ad4289
Nuri C <nc1813@imperial.ac.uk>
2015-05-30 10:45:38 +0100

    Split event and calendar js to different files

---
+++ server/webapps/ROOT/js/main_calendar.js
@@ -0,0 +1,139 @@
+// Document Ready
+$(function() {
+  // Bind calendar creation form
+  $("#calendar_create_form").submit(function(e) {
+    e.preventDefault()
+    submitAjaxForm($(this), function(data) { toastr.success(data.name + " created!"); refreshCalendars(); }, $("#calendar_create_errors"));
+  });
+  
+  // Bind calendar joining form
+  $("#calendar_follow_form").submit(function(e) {
+    e.preventDefault()
+    submitAjaxForm($(this), function(data) { toastr.success("You started following " + data.name); refreshCalendars(); }, $("#calendar_follow_errors"));
+  });
+  
+  // Bind edit calendar buttons and forms
+  $("#b_cancel_calendar").click(function() {
+    $("#d_edit_calendar").toggle();
+    $("#d_user_calendars").toggle();
+  });
+  
+  $("#b_delete_calendar").click(function() {
+    var calid = $(this).parent().data("calid");
+    var name = $.grep(app.calendars, function(e){ return e.calendarId == calid; })[0].name;
+    if(confirm("Are you sure you want to delete "+name+"?")){
+      $.ajax("/api/calendar/"+calid, {
+        method: "DELETE",
+        success: function(data) {
+          toastr.warning("Deleted " + name);
+          $("#b_cancel_calendar").click(); // hide this window again
+          refreshCalendars();
+        },
+        error: function(data) {
+          toastr.error("Could not delete " + name);
+          refreshCalendars();
+        }
+      });
+    }
+  });
+  
+  $("#user_promotion_form").submit(function(e) {
+    e.preventDefault()
+    submitAjaxForm($(this), function(data) { toastr.success("Updated user"); $("#b_cancel_calendar").click(); }, $("#user_promotion_errors"));
+  });
+  
+  $("#calendar_edit_form").submit(function(e) {
+    e.preventDefault()
+    submitAjaxForm($(this), function(data) { toastr.success("Updated user"); $("#b_cancel_calendar").click(); }, $("#calendar_edit_errors"));
+  });
+  
+}); // End of document ready
+
+// Update calendars
+function refreshCalendars() {
+  var cal_html = '<div data-calid="{{id}}" class="calendar"> \
+  <div class="checkbox"> \
+    <label> \
+      <input type="checkbox"> {{name}} \
+    </label> \
+  </div> \
+  <div class="calendar-extras"> \
+    <p>Join code: <strong>{{joinCode}}</strong></p> \
+    <p>Join enabled: <strong>{{joinEnabled}}</strong></p> \
+    <button type="button" class="btn btn-info">Edit</button> \
+  </div> \
+</div>';
+  $.get("/api/subscription/calendar", function(data) {
+    app.calendars = data.calendars;
+    if (data.calendars.length < 1) {
+      return;
+    }
+    $("#d_user_calendars").empty();
+    $("#select_calendar").empty();
+    $.each(data.calendars, function(index, calendar) {
+      $('#select_calendar')
+       .append($("<option></option>")
+       .attr("value",calendar.calendarId)
+       .text(calendar.name));
+       
+      var cal_div = $(cal_html.replace("{{id}}", calendar.calendarId)
+         .replace("{{name}}", calendar.name)
+         .replace("{{joinCode}}", calendar.joinCode)
+         .replace("{{joinEnabled}}", calendar.joinEnabled)).appendTo("#d_user_calendars");
+      
+      cal_div.find("input").change(refreshEvents);
+      cal_div.find("button").click(function() {
+        var calid = $(this).parent().parent().data("calid");
+        $("#d_edit_calendar input[name='name']").val(calendar.name);
+        $("#d_edit_calendar input[type='checkbox']").prop("checked", calendar.joinEnabled);
+        $("#d_user_calendars").toggle();
+        $("#d_edit_calendar").data("calid", calid).toggle();
+      });
+    });
+    // Refresh events for the calendars
+    $("#d_user_calendars input").first().prop("checked", "true");
+    refreshEvents();
+  });
+}
+
+// Update data-date field of calendar view from startDate
+function updateCalendarDates(startDate) {
+  // TODO: Handle different time zone
+  app.current_start_date = new Date(startDate);
+  $("#prev_day").next().text(formatDate(startDate));
+
+  $("#t_calendar_body").children().each(function(k, elem) {
+    // update data fields
+    var date = startDate.toJSON().split("T")[0];
+    $(elem).data("date", date);
+
+    // update heading text
+    var heading = $($("#t_calendar_heading").children()[k]);
+    heading.text(getWeekDay(startDate));
+
+    // update heading class
+    heading.removeClass("th_weekend").removeClass("th_weekday");
+    var day = startDate.getDay();
+    if (day === 0 || day === 6) {
+      heading.addClass("th_weekend");
+    } else {
+      heading.addClass("th_weekday");
+    }
+
+    // increment date
+    startDate.setDate(startDate.getDate() + 1);
+  });
+
+  $("#next_day").prev().text(formatDate(startDate));
+}
+
+// Get active_calendar ids
+function getActiveCalendarIds() {
+  var active_calendars = [];
+  $("#calendars_collapse .calendar").each(function(index) {
+    if ($(this).find("input").is(":checked")) {
+      active_calendars.push($(this).data("calid"));
+    }
+  });
+  return active_calendars;
+}
---
+++ server/webapps/ROOT/js/main_event.js
@@ -0,0 +1,303 @@
+// Document Ready
+$(function() {
+  // Bind datetime picker
+  $(".datetimepicker").datetimepicker();
+
+  // Bind edit event buttons
+  $("#btn_event_save").click(function() {
+    // Retrieve form data
+    var form = $("#event_form");
+    var formObj = getFormObj(form);
+    if (!formObj.eventId) {
+      toastr.error("Failed to read event id. Please select the event again or refresh the app.");
+      return;
+    }
+
+    // validate form
+    formatEventForm(formObj);
+    console.log(formObj);
+
+    // ajax put
+    $.ajax(form.attr("action") +"/"+formObj.eventId, {
+      method: "PUT",
+      data: JSON.stringify(formObj),
+      success: function(data) {
+        toastr.success("Saved chanages to " + formObj["title"]);
+        refreshEvents();
+        form.trigger("reset");
+        $("#btn_event_create").show();
+        $("#btn_event_save").hide();
+        $("#btn_event_delete").hide();
+        $("#btn_event_cancel").hide();
+      },
+      error: function(data) { $("#event_create_errors").text(data.responseJSON.message); }
+    })
+  });
+
+  $("#btn_event_cancel").click(function() {
+    $("#event_form").trigger("reset");
+    $("#btn_event_create").show();
+    $("#btn_event_save").hide();
+    $("#btn_event_delete").hide();
+    $("#btn_event_cancel").hide();
+  });
+
+  $("#btn_event_delete").click(function() {
+    // Retrieve form data
+    var form = $("#event_form");
+    var formObj = getFormObj(form);
+    if (!formObj.eventId) {
+      toastr.error("Failed to read event id. Please select the event again or refresh the app.");
+      return;
+    }
+
+    // ajax delete
+    $.ajax(form.attr("action") +"/"+formObj.eventId, {
+      method: "DELETE",
+      success: function(data) {
+        toastr.success("Saved chanages to " + formObj["title"]);
+        refreshEvents();
+        form.trigger("reset");
+        $("#btn_event_create").show();
+        $("#btn_event_save").hide();
+        $("#btn_event_delete").hide();
+        $("#btn_event_cancel").hide();
+      },
+      error: function(data) { $("#event_create_errors").text(data.responseJSON.message); }
+    });
+  });
+
+  // Bind event creation form
+  $("#event_form").submit(function(e) {
+    e.preventDefault();
+    // Seperate datetime into date and time
+    if ($("#event_form select").val() < 0) {
+      $("#event_create_errors").text("You must create a calendar first.");
+      return;
+    }
+    var form = $(this);
+    var formObj = getFormObj(form);
+    formatEventForm(formObj);
+    
+    // Submit form
+    $.ajax(form.attr("action"), {
+      data: JSON.stringify(formObj),
+      method: form.attr("method"),
+      success: function(data) {
+        toastr.success("Created " + formObj["title"]);
+        refreshEvents();
+        form.trigger("reset");
+      },
+      error: function(data) { $("#event_create_errors").text(data.responseJSON.message); }
+    });
+  });
+  
+}); // End of document ready
+
+// Update Events
+function refreshEvents() {
+  // Retrieve and render calendar events
+  app.events = [];
+  var active_calendars = getActiveCalendarIds();
+  // Just re-render if there are no active calendars
+  if (active_calendars.length < 1) {
+    updateCalendarDates(app.current_start_date);
+    renderEvents();
+  }
+  // Get event data for the active calendars then render
+  $.each(active_calendars, function(index, id) {
+    $.ajax("/api/calendar/"+id, {
+      data: {startDate: app.current_start_date.toJSON().split('T')[0] + " 00:00:00"},
+      success: function(data) {
+        // Add the calendarId because back-end doesn't provide it
+        $.each(data.events, function(index, event) {
+          event.calendarId = id;
+        });
+        app.events.push.apply(app.events, data.events);
+        updateCalendarDates(app.current_start_date);
+        renderEvents();
+      },
+      error: function(data) {
+        toastr.error("Failed to get events for " + id);
+      }
+    });
+  });
+  
+}
+
+// Render events
+function renderEvents() {
+  var active_calendars = getActiveCalendarIds();
+  // Clear any existing events
+  $("#t_calendar_body").children().each(function(index) {
+    $(this).empty();
+  });
+  
+  // Rerender active calendars' events
+  $.each(app.events, function(index, event) {
+    if (active_calendars.indexOf(event.calendarId) >= 0) {
+      createEventView(event);
+    }
+  });
+}
+
+// Render a new event on the calendar
+function createEventView(event) {
+  // expand description on hover
+  // find the cell corresponding to start date
+  var temp =
+  '<div data-event-id="{{eventId}}" class="event">'+
+    '<div class="time">'+
+      '<dd>{{startDate}}</dd>'+
+      '<dd>{{startTime}}</dd>'+
+    '</div>'+
+    '<div class="header progress-bar-info" onclick="editEvent(this)">'+
+      '<span class="label label-warning count">{{remaining}}</span>'+
+    '</div>'+
+    '<div class="title">{{title}}</div>'+
+    '<div class="desc">{{description}}</div>'+
+    '<div class="location">'+
+      '<span class="glyphicon glyphicon-map-marker"></span> {{location}}'+
+    '</div>'+
+    '<div class="join">'+
+      '<a class="badge" onclick="joinEvent(this)">Join</a>'+
+    '</div>'+
+  '</div>';
+  $("#t_calendar_body").children().each(function(k, elem) {
+    if ($(elem).data("date") === event.startDate) {
+      var startDateTime = new Date(event.startDate + "T" + event.startTime.split("+")[0]);
+      var readableDate = formatDate(startDateTime).split(" ").reverse().join(" ");
+      var readableTime = startDateTime.toLocaleTimeString().substring(0, 5);
+      // append event div
+      temp = temp
+        .replace('{{eventId}}', event.eventId)
+        .replace('{{startDate}}', readableDate)
+        .replace('{{startTime}}', readableTime)
+        .replace('{{title}}', event.title)
+        .replace('{{description}}', event.description)
+        .replace('{{location}}', event.location);
+      
+      if (event.max == -1) {
+        temp = temp.replace('{{remaining}}', "&infin;");
+      } else {
+        temp = temp.replace('{{remaining}}', event.max - event.currentCount);
+      }
+      var view = $(temp);
+      $(elem).append(view);
+      if (event.hasJoined) {
+        // update joined badge
+        view.find(".badge").addClass("progress-bar-danger").text("Unjoin");
+        // update header
+        view.find(".header").removeClass("progress-bar-info").addClass("progress-bar-success");
+      } else if (event.max - event.currentCount == 0) {
+        view.find(".badge").css("visibility", "hidden");
+      }
+      // hide location if it is not set
+      if (!event.location) {
+        view.find(".location").hide();
+      }
+    }
+    // if event not in view, don't render
+  });
+}
+
+// Join an event
+function joinEvent(elem) {
+  var view = $(elem).closest(".event");
+  var eid = view.data("eventId");
+  var event = $.grep(app.events, function(e){ return e.eventId == eid; })[0];
+  
+  // determine wether to join or unjoin
+  if (event.hasJoined) {
+    // unjoin an event
+    $.ajax("/api/subscription/event", {
+      method: "DELETE",
+      data: JSON.stringify({eventId: eid}),
+      success: function(data) {
+        event.hasJoined = false;
+        toastr.warning("Unjoined event " + event.title);
+        if (event.max > -1) {
+          // update remaining spots
+          event.currentCount -= 1;
+          view.find(".count").text(event.max - event.currentCount);
+        }
+        // update badge
+        $(elem).removeClass("progress-bar-danger").text("Join");
+        // update header
+        view.find(".header").removeClass("progress-bar-success").addClass("progress-bar-info");
+      },
+      error: function(data) {
+        toastr.error("Cannot join event: " + data.responseJSON.message);
+        refreshEvents();
+      }
+    });
+  } else {
+    // join an event if there are spaces left
+    if ( event.max > 0 && event.max - event.currentCount < 1) {
+      toastr.error(event.title + " is full");
+      return;
+    }
+    if (event.max == 0) {
+      toastr.error("Joining for " + event.title + " is disabled");
+      return;
+    }
+    $.ajax("/api/subscription/event", {
+      method: "POST",
+      data: JSON.stringify({eventId: eid}),
+      success: function(data) {
+        toastr.success("Joined event " + event.title);
+        // use dictionary to prevent duplicates
+        event.hasJoined = true;
+        // update remaining spots
+        event.currentCount += 1;
+        if (event.max > -1) {
+          view.find(".count").text(event.max - event.currentCount);
+        }
+        // update badge
+        $(elem).addClass("progress-bar-danger").text("Unjoin");
+        // update header
+        view.find(".header").removeClass("progress-bar-info").addClass("progress-bar-success");
+      },
+      error: function(data) {
+        toastr.error("Cannot unjoin event: " + data.responseJSON.message);
+        refreshEvents();
+      }
+    });
+  }
+}
+
+// Handler for editing event
+function editEvent(elem) {
+  var view = $(elem).closest(".event");
+  var eid = view.data("eventId");
+  var event = $.grep(app.events, function(e){ return e.eventId == eid; })[0];
+
+  // update event editor
+  $("#btn_event_create").hide();
+  $("#btn_event_save").show();
+  $("#btn_event_delete").show();
+  $("#btn_event_cancel").show();
+
+  // unformat and populate
+  var startDateTime = event.startDate.replace("-", "/") + " " + event.startTime;
+  //console.log(event);
+
+  var form = $("#event_form");
+  form.find('input[name="title"]').val(event.title);
+  form.find('input[name="description"]').val(event.description);
+  form.find('input[name="startDate"]').val(startDateTime);
+  //form.find('input[name="endDate"]').val(event.endDate);
+  form.find('input[name="location"]').val(event.location);
+  form.find('input[name="max"]').val(event.max);
+  form.find('input[name="eventId"]').val(event.eventId);
+  form.find('select[name="calendarId"]').val(event.calendarId);
+}
+
+function formatEventForm(formObj) {
+  var regex = new RegExp('/', "g");
+  formObj["startTime"] = formObj["startDate"].split(" ")[1];
+  formObj["startDate"] = formObj["startDate"].split(" ")[0].replace(regex, '-');
+  formObj["endTime"] = formObj["endDate"].split(" ")[1];
+  formObj["endDate"] = formObj["endDate"].split(" ")[0].replace(regex, '-');
+  formObj["timezone"] = jstz.determine().name();
+}
