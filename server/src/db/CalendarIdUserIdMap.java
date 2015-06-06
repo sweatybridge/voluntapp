589ff7d868101a5e3f030109341a121eaf3820c1
Piotr Chabierski <pc3813@ray11.doc.ic.ac.uk>
2015-06-06 13:43:47 +0100

    Added a map for storing calendar subscriptions of logged in users.

---
+++ server/src/db/CalendarIdUserIdMap.java
@@ -0,0 +1,79 @@
+package db;
+
+import java.sql.SQLException;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+
+import exception.InconsistentDataException;
+
+import resp.CalendarResponse;
+import resp.CalendarSubscriptionResponse;
+
+import utils.ConcurrentHashSet;
+
+public class CalendarIdUserIdMap {
+  
+  private DBInterface db;
+  private static CalendarIdUserIdMap instance;
+  
+  /* Map from calendar IDs to list (set) of user IDs subscribed to the certain 
+   * calendar.
+   */
+  private ConcurrentMap<Integer, ConcurrentHashSet<Integer>> map;
+  
+  private CalendarIdUserIdMap() {
+    map = new ConcurrentHashMap<Integer, ConcurrentHashSet<Integer>>();
+  }
+  
+  public static synchronized CalendarIdUserIdMap getInstance() {
+    if (instance == null) {
+      instance = new CalendarIdUserIdMap();
+    }
+    return instance;
+  }
+  
+  public void put(Integer calendarId, Integer userId) {
+    if (!map.containsKey(calendarId)) {
+      map.put(calendarId, new ConcurrentHashSet<Integer>());
+    }
+    map.get(calendarId).add(userId);
+  }
+  
+  /**
+   * Given the calendar ID, return the array of calendar subscribers.
+   *  
+   * @param  calendar ID of the queried calendar 
+   * @return the array of user IDs (users subscribed to a given calendar)
+   */
+  public Integer[] getUserIds(Integer calendarId) {
+    ConcurrentHashSet<Integer> set = map.get(calendarId);
+    if (set != null) {
+      return ((Integer[]) set.toArray());
+    }
+    return null;
+  }
+  
+  /** 
+   * Called when the user logs out to remove his calendar subscriptions from
+   * the map.
+   * 
+   * @param user ID of the user which logs out form the system
+   */
+  public boolean deleteUser(int userId) {
+    try {
+      CalendarSubscriptionResponse resp = db.getUsersCalendars(userId);
+      for (CalendarResponse calendar : resp.getCalendars()) {
+        int calendarId = calendar.getCalendarId();
+        ConcurrentHashSet<Integer> set = map.get(calendarId);
+        if (set != null) {
+          set.remove(userId);
+        }
+        return true;
+      }
+    } catch (SQLException | InconsistentDataException e) {
+      System.err.println("Error while removing data from the map.");
+    }
+    return false;
+  }
+  
+}
