68e61ff57715a937fcba8f971982ae273ebacf8c
Han Qiao <qh812@imperial.ac.uk>
2015-06-07 17:43:50 +0100

    fixed makefile, putting properties file in resource pacakge

--- server/src/utils/DBParameters.java
+++ server/src/utils/DBParameters.java
@@ -4,7 +4,7 @@ import java.util.MissingResourceException;
 import java.util.ResourceBundle;
 
 public class DBParameters {
-  private static final String BUNDLE_NAME = "utils.db"; //$NON-NLS-1$
+  private static final String BUNDLE_NAME = "resource.db"; //$NON-NLS-1$
 
   private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle
       .getBundle(BUNDLE_NAME);
