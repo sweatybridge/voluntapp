ee4723c1efe15f2713a532d10f26bc57ecab6d19
Nuri C <nc1813@imperial.ac.uk>
2015-06-08 11:30:29 +0100

    Added comments for DynamicUpdate and MessageType

--- server/src/chat/DynamicUpdate.java
+++ server/src/chat/DynamicUpdate.java
@@ -7,29 +7,82 @@ import db.CalendarIdUserIdMap;
 import resp.CalendarResponse;
 import resp.EventResponse;
 
+/**
+ * Sends dynamic updates to the calendar subscribers through the chat server. It
+ * uses the CalendarIdUserIdMap to find which users should be notified.
+ * 
+ * @author nc1813
+ * @author pc3813
+ * 
+ */
 public class DynamicUpdate {
-  
+
+  /**
+   * Given the calendarId, it sends the obj to all of its ONLINE subscribers
+   * given an MessageType
+   * 
+   * @param calendarId
+   *          CalendarId of which the ONLINE subscribers will be notified
+   * @param mType
+   *          The type of the ChatMessage
+   * @param obj
+   *          The object that is going to be sent
+   */
   private static void sendObj(Integer calendarId, MessageType mType, Object obj) {
     CalendarIdUserIdMap map = CalendarIdUserIdMap.getInstance();
-    ChatMessage cm = new ChatMessage(mType.getType(),
-        Arrays.asList(map.getUserIds(calendarId)), -1, new Date(), false, obj);
+    ChatMessage cm = new ChatMessage(mType.getType(), Arrays.asList(map
+        .getUserIds(calendarId)), -1, new Date(), false, obj);
     ChatServer.routeChatMessage(cm);
   }
-  
+
+  /**
+   * Sends an "update/event" to subscribers to the calendar
+   * 
+   * @param calendarId
+   *          CalendarId of which the ONLINE subscribers will be notified
+   * @param event
+   *          Event object to be sent
+   */
   public static void sendEventUpdate(Integer calendarId, EventResponse event) {
     sendObj(calendarId, MessageType.EVENT_UPDATE, event);
   }
-  
+
+  /**
+   * Sends a "delete/event" to subscribers to the calendar
+   * 
+   * @param calendarId
+   *          CalendarId of which the ONLINE subscribers will be notified
+   * @param event
+   *          Event object to be sent
+   */
   public static void sendEventDelete(Integer calendarId, EventResponse event) {
     sendObj(calendarId, MessageType.EVENT_DELETE, event);
   }
-  
-  public static void sendCalendarDelete(Integer calendarId, CalendarResponse calendar) {
+
+  /**
+   * Sends a "delete/calendar" to subscribers to the calendar
+   * 
+   * @param calendarId
+   *          CalendarId of which the ONLINE subscribers will be notified
+   * @param calendar
+   *          CalendarResponse to be sent
+   */
+  public static void sendCalendarDelete(Integer calendarId,
+      CalendarResponse calendar) {
     sendObj(calendarId, MessageType.CALENDAR_DELETE, calendar);
   }
-  
-  public static void sendCalendarUpdate(Integer calendarId, CalendarResponse resp) {
-    sendObj(calendarId, MessageType.CALENDAR_UPDATE, resp);
+
+  /**
+   * Sends a "update/calendar" to subscribers to the calendar
+   * 
+   * @param calendarId
+   *          CalendarId of which the ONLINE subscribers will be notified
+   * @param calendar
+   *          CalendarResponse to be sent
+   */
+  public static void sendCalendarUpdate(Integer calendarId,
+      CalendarResponse calendar) {
+    sendObj(calendarId, MessageType.CALENDAR_UPDATE, calendar);
   }
-  
+
 }
--- server/src/chat/MessageType.java
+++ server/src/chat/MessageType.java
@@ -1,18 +1,23 @@
 package chat;
 
+/**
+ * Enumerates ChatMessage types for DynamicUpdate sent to the subscribers
+ * 
+ * @author nc1813
+ * @author pc3813
+ * 
+ */
 public enum MessageType {
-  EVENT_UPDATE("update/event"),
-  EVENT_DELETE("delete/event"),
-  
-  CALENDAR_UPDATE("update/calendar"),
-  CALENDAR_DELETE("delete/calendar");
-  
+  EVENT_UPDATE("update/event"), EVENT_DELETE("delete/event"),
+
+  CALENDAR_UPDATE("update/calendar"), CALENDAR_DELETE("delete/calendar");
+
   private String type;
-  
+
   private MessageType(String type) {
     this.type = type;
   }
-  
+
   public String getType() {
     return type;
   }
